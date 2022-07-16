0cd62307c9cd0f4ccb9351e2d35ce695b3cfa7ee
Nicoli1977 <86806874+Gomezzzer@users.noreply.github.com>
2022-07-15 19:57:40 -0500

    revised

---
+++ tabs.js
@@ -0,0 +1,22 @@
+
+const tabList = document.querySelector('[role="tablist"]');
+const tabs = tabList.querySelectorAll('[role="tab"]');
+
+tabList.addEventListener('keydown', (e) => {
+    const keydownLeft = 37;
+    const keydownRight = 39;
+
+    if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
+        tabs[tabFocus].setAttribute("tabindex", -1);
+    }
+    
+    if (e.keyCode === keydownRight) {
+        tabFocus++;
+    }
+    
+    if (e.keyCode === keydownLeft) {
+        tabFocus--;
+    }
+
+
+})
